87e308a4431445758f05e303bb4075f3d6b76e68
Chase Jensen <soccerchase@gmail.com>
2018-12-26 09:51:28 -0700

    Wire up redux

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const App = () => {
+  return (
+    <Provider store={createStore}>
+      <View />
+    </Provider>
+  );
+};
+
+export default App;
