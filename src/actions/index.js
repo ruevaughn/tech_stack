0bfc35deb92afdbee7a2042229ffbc065769aceb
Chase Jensen <soccerchase@gmail.com>
2019-01-08 13:41:30 -0700

    Wire up action creator to list component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // Action Creator is a JS function
+// Actions tell reducers to update in a very specific way
 export const selectLibrary = (libraryId) => {
   return {
     type: 'select_library',
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
+
+    console.log(this.props);
+
     return (
       <CardSection>
         <Text style={titleStyle}>
           {this.props.library.item.title}
         </Text>
-      </CardSection>
+      </ CardSection >
     );
   }
 }
@@ -25,4 +30,7 @@ const styles = {
   }
 };
 
-export default ListItem;
+// connect helper from redux modifies what data shows up as props.
+// the first argument is always MapStateToProps if you are wanting it
+// null if not
+export default connect(null, actions)(ListItem);
