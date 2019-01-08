773d7d3fc83d898f52b1ffceb36d53c9d9e117da
Chase Jensen <soccerchase@gmail.com>
2019-01-08 11:13:47 -0700

    Start implementing ListItem component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
 
 class ListItem extends Component {
   render() {
-      
+    return (
+      <CardSection>
+        <Text>{this.props.library.item.title}</Text>
+      </CardSection>
+    );
   }
 }
 
